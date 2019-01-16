d1bf020e3d2e2c3b1be1eeccf80a7c9f97585a5c
Adria Navarro <anavarromartin@pivotal.io>
2019-01-16 15:28:39 +0100

    changes

--- test/testdouble/MissileLauncherTest.java
+++ test/testdouble/MissileLauncherTest.java
@@ -1,10 +1,18 @@
 package testdouble;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import static testdouble.MissileLauncher.launchMissile;
 
 public class MissileLauncherTest {
+
+    private LaunchCode expiredLaunchCode = new ExpiredLaunchCode();
+
+    @Before
+    public void setUp() {
+
+    }
 
     @Test
     public void givenExpiredLaunchCodes_missileIsNotLaunched() {

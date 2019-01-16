442fc61dc631fac0b56a90862564df2bc9744a40
Adria Navarro <anavarromartin@pivotal.io>
2019-01-16 10:58:42 +0100

    fake tested, main test failing

--- test/testdoubles/FakeUsedLaunchCodes.java
+++ test/testdoubles/FakeUsedLaunchCodes.java
@@ -1,13 +1,17 @@
 package testdoubles;
 
+import java.util.HashSet;
+
 public class FakeUsedLaunchCodes implements UsedLaunchCodes {
+    private HashSet<LaunchCode> set = new HashSet<>();
+
     @Override
     public boolean contains(LaunchCode launchCode) {
-        return false;
+        return set.contains(launchCode);
     }
 
     @Override
     public void add(LaunchCode launchCode) {
-
+        set.add(launchCode);
     }
 }

07788a6db79d38b935610f06e7bdb925040f17a1
Adria Navarro <anavarromartin@pivotal.io>
2019-01-16 12:23:21 +0100

    changes

---
+++ src/testdoubles/Missile.java
@@ -0,0 +1,5 @@
+package testdoubles;
+
+interface Missile {
+    void launch();
+}
--- test/testdoubles/MissileLauncherTest.java
+++ test/testdoubles/MissileLauncherTest.java
@@ -1,8 +1,6 @@
 package testdoubles;
 
 import org.junit.Test;
-import testdoubles.MissileLauncher.LaunchCode;
-import testdoubles.MissileLauncher.Missile;
 
 import static org.junit.Assert.assertFalse;
 import static testdoubles.MissileLauncher.launchMissile;
